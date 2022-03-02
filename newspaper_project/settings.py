fb4d8773ba1ed3d7035cc40c23b304d7df2c6179
Frax600 <frax600@gmail.com>
2022-03-02 10:20:57 +0100

    Añade ficheros de configuración Heroku

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^zcojy^j^^jhi5)swm532&b2kw^frv^+vpcl@c06ovtko(r29m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
